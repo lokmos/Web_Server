e5cf223b4275052dd4bb362cd6c0b7bc5c19168a
lokmos <23110240153@m.fudan.edu.cn>
2024-07-01 01:54:21 -0700

    log update

--- log/block_queue.h
+++ log/block_queue.h
@@ -8,7 +8,7 @@
 #include "../lock/locker.h"
 using namespace std;
 
-template <typename T>
+template <class T>
 class block_queue {
 public:
     block_queue(int max_size=1000) {
@@ -24,7 +24,7 @@ public:
 
     ~block_queue() {
         m_mutex.lock();
-        delete []m_array;
+        delete [] m_array;
         m_mutex.unlock();
     }
 
@@ -54,6 +54,74 @@ public:
         }
         m_mutex.unlock();
         return false;
+    }
+
+    bool front(T &value) {
+        m_mutex.lock();
+        if (m_size == 0) {
+            m_mutex.unlock();
+            return false;
+        }
+        value = m_array[m_front];
+        m_mutex.unlock();
+        return true;
+    }
+
+    bool back(T &value) {
+        m_mutex.lock();
+        if (m_size == 0) {
+            m_mutex.unlock();
+            return false;
+        }
+        value = m_array[m_back];
+        m_mutex.unlock();
+        return true;
+    }
+
+    int size() {
+        int tmp = 0;
+        m_mutex.lock();
+        tmp = m_size;
+        m_mutex.unlock();
+        return tmp;
+    }
+
+    int max_size() {
+        int tmp = 0;
+        m_mutex.lock();
+        tmp = m_max_size;
+        m_mutex.unlock();
+        return tmp;
+    }
+
+    bool push(const T &item) {
+        m_mutex.lock();
+        if (m_size >= m_max_size) {
+            m_cond.broadcast();
+            m_mutex.unlock();
+            return false;
+        }
+        m_back = (m_back + 1) % m_max_size;
+        m_array[m_back] = item;
+        m_size++;
+        m_cond.broadcast();
+        m_mutex.unlock();
+        return true;
+    }
+
+    bool pop(T &item) {
+        m_mutex.lock();
+        while (m_size <= 0) {
+            if (!m_cond.wait(m_mutex.get())) {
+                m_mutex.unlock();
+                return false;
+            }   
+        }
+        m_front = (m_front + 1) % m_max_size;
+        item = m_array[m_front];
+        m_size--;
+        m_mutex.unlock();
+        return true;
     }
     
 private:
