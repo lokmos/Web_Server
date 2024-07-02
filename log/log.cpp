400a5cf708de04aeb418018644ebb5a22f93df52
lokmos <23110240153@m.fudan.edu.cn>
2024-07-02 01:29:12 -0700

    log update

--- log/log.cpp
+++ log/log.cpp
@@ -64,4 +64,79 @@ bool Log::init(const char *file_name, int close_log, int log_buf_size = 8192, in
 void Log::write_log(int level, const char *format, ...) {
     struct timeval now = {0, 0};
     gettimeofday(&now, NULL);
+    time_t t = now.tv_sec;
+    struct tm *sys_tm = localtime(&t);
+    struct tm_my_tm = *sys_tm;
+
+    char s[16] = {0};
+
+    switch (level) {
+        case 0:
+            strcpy(s, "[debug]:");
+            break;
+        case 1:
+            strcpy(s, "[info]:");
+            break;
+        case 2:
+            strcpy(s, "[warn]:");
+            break;
+        case 3:
+            strcpy(s, "[error]:");
+            break;
+        default:
+            strcpy(s, "[info]:");
+            break;
+    }
+
+    m_mutex.lock();
+
+    m_count++;
+
+    if (m_today != my_tm.tm_mday || m_count % m_split_lines == 0) {
+        fflush(m_fp);
+        fclose(m_fp);
+        char new_log[256] = {0};
+        char tail[16] = {0};
+
+        snprintf(tail, 16, "%d_%02d_%02d_", my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday);
+
+        if (m_today != my_tm.tm_mday) {
+            snprintf(new_log, 255, "%s%s%s", dir_name, tail, log_name);
+            m_today = my_tm.tm_mday;
+            m_count = 0;
+        }
+        else {
+            snprintf(new_log, 255, "%s%s%s.%lld", dir_name, tail, log_name, m_count / m_split_lines);
+        }
+    }
+
+    m_mutex.unlock();
+
+    va_list valst;
+    va_start(valst, format);
+
+    char log_str[256] = {0};
+
+    mutex.lock();
+    int n = snprintf(m_buf, 48, "%d-%02d-%02d %02d:%02d:%02d.%06ld %s ",
+                     my_tm.tm_year + 1900, my_tm.tm_mon + 1, my_tm.tm_mday,
+                     my_tm.tm_hour, my_tm.tm_min, my_tm.tm_sec, now.tv_usec, s);
+
+    int m = vsnprintf(m_buf + n, m_log_buf_size - n - 1, format, valst);
+    m_buf[n + m] = '\n';
+    m_buf[n + m + 1] = '\0';
+    log_str = m_buf;
+
+    m_mutex.unlock();
+
+    if (m_is_async && !m_log_queue->full()) {
+        m_log_queue->push(log_str);
+    }
+    else {
+        m_mutex.lock();
+        fputs(log_str.c_str(), m_fp);
+        m_mutex.unlock();
+    }
+
+    va_end(valst);
 }
